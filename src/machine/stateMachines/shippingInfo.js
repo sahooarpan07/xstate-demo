1d6da8c4df4d313864d8f029d32f3a2b3f1c486b
sahooarpan07 <arpan.sahoo@publicissapient.com>
2022-10-11 19:08:44 +0530

    Made shipment flow hierarchical

--- src/machine/stateMachines/shippingInfo.js
+++ src/machine/stateMachines/shippingInfo.js
@@ -1,21 +1,46 @@
 import { assign } from "xstate";
 export const enterShippingInfo = {
-  on: {
-    BACK: {
-      target: "enterPaymentDetails",
-      actions: assign({
-        shippingInfo: (_context, event) => [],
-      }),
+  initial: "captureShippingInfo",
+  states: {
+    captureShippingInfo: {
+      on: {
+        BACK: {
+          target: "#workFlow.enterPaymentDetails",
+          actions: assign({
+            shippingInfo: (_context, event) => [],
+          }),
+        },
+        NEXT: {
+          target: "confirmShouldAddMoreShippingInfo",
+          actions: assign({
+            shippingInfo: (_context, event) => [
+              event.data,
+              ..._context.shippingInfo,
+            ],
+          }),
+        },
+      },
     },
-    NEXT: {
-      target: "addMoreShippingInfo",
-
-      actions: assign({
-        shippingInfo: (_context, event) => [
-          event.data,
-          ..._context.shippingInfo,
+    confirmShouldAddMoreShippingInfo: {
+      on: {
+        NEXT: [
+          {
+            target: "captureShippingInfo",
+            cond: (_context, event) => {
+              return event.data === "yes";
+            },
+          },
+          {
+            target: "#workFlow.summary",
+            cond: (_context, event) => {
+              return event.data === "no";
+            },
+          },
         ],
-      }),
+        BACK: {
+          target: "captureShippingInfo",
+        },
+      },
     },
   },
 };
